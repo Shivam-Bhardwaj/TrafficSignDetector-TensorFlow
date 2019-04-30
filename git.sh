200ffd6bde08fccf669741b3a7bb5d728519ed7d
shivam-bhardwaj <sb6377@nyu.edu>
2019-04-30 03:38:38 -0400

    Easy git update

--- git.sh
+++ git.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#if [ -z "${VARIABLE}" ]; then 
-#    message='Random Updates'
-#else 
-#    message=${VARIABLE}
-#fi
-
-#exp=${exp:-Minor updates}
-
-
 git add .
 git commit -m "$1"
 git push
