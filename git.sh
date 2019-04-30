de882d5f78df3dddfdc95357b7a298a1e0c95ad2
shivam-bhardwaj <sb6377@nyu.edu>
2019-04-30 03:17:41 -0400

    VAR1

--- git.sh
+++ git.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -z "${VARIABLE}" ]; then 
-    message='Random Updates'
-else 
-    message=${VARIABLE}
-fi
+#if [ -z "${VARIABLE}" ]; then 
+#    message='Random Updates'
+#else 
+#    message=${VARIABLE}
+#fi
+
+VAR1="${VAR1:-"Minor updates"}"
 
 git add .
-git commit -m FOO
+git commit -m VAR1
 git push
 
